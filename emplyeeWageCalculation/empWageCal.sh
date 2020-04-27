c1be7c66db7749395bd15669ad5db96e2f3ffc64
“Shubham” <shubhampattar2@gmail.com>
2020-04-27 13:03:06 +0530

    All Code Combined as Final code And ready to push to Develop and Master

--- emplyeeWageCalculation/empWageCal.sh
+++ emplyeeWageCalculation/empWageCal.sh
@@ -11,4 +11,14 @@ else
 	workingDays=20;
 	monthlyWage=0;
 	totalWorkedHR=0;
+	for(( i=0; i<=$workingDays; i++ ))
+	do
+		worked=$((RANDOM%9));
+		dailyWage=$((worked*wagePerHr));
+		monthlyWage=$((monthlyWage+dailyWage));
+		totalWorkedHR=$((totalWorkedHR+worked));
+
+	done
+	echo "Employee's total working Hours in this month "$totalWorkedHR;
+	echo "Employee's Monthly Wage : "$monthlyWage;
 fi
